d81cb6a9ebb11b09bcb42dd3dcbb02fe4da91c2e
Jordan Stapinski <jordan.stapinski@mongodb.com>
2025-02-04 22:44:45 -0500

    First pass upgrade to v3, css not working because CORS

---
+++ service_worker.js
@@ -0,0 +1,70 @@
+chrome.storage.sync
+  .get({
+    match: "https://my-source.com",
+    replace: "http://localhost:8080",
+    compressed: false,
+  })
+  .then(async ({ match, replace, compressed }) => {
+    const replaceURL = new URL(replace);
+
+    const responseHeaders = [
+      { header: "Access-Control-Allow-Origin", operation: "set", value: "*" },
+      { header: "Access-Control-Allow-Headers", operation: "set", value: "*" },
+    ];
+
+    console.log(chrome.webRequest.OnHeadersReceivedOptions.EXTRA_HEADERS);
+
+    const urlWithDefaultPort = `${replaceURL.protocol}//${replaceURL.hostname}:8080`;
+    const isCompressed = compressed === "true";
+
+    const jsRegex = `(account|admin|auth|main|project|ecosystem)(\.[a-fA-F0-9]*)${
+      isCompressed ? ".min" : ""
+    }(\.js)$`;
+    const cssRegex = `(\.[a-f0-9]{10})(?:\.min)?(\.css)`;
+
+    const oldRules = await chrome.declarativeNetRequest.getDynamicRules();
+    const oldRuleIds = oldRules.map((rule) => rule.id);
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: oldRuleIds,
+      addRules: [
+        {
+          id: 1,
+          action: {
+            type: "redirect", // css
+            redirect: {
+              regexSubstitution: `${urlWithDefaultPort}\\1\\3`,
+            },
+          },
+          condition: {
+            regexFilter: `${match}(.*)${cssRegex}`,
+            resourceTypes: ["stylesheet", "xmlhttprequest"],
+          },
+        },
+        {
+          id: 2,
+          action: {
+            type: "redirect", // js
+            redirect: {
+              regexSubstitution: `${replace}\\1\\2\\4`,
+            },
+          },
+          condition: {
+            regexFilter: `${match}(.*)${jsRegex}`,
+            resourceTypes: ["script", "xmlhttprequest"],
+          },
+        },
+        {
+          id: 3,
+          action: {
+            type: "modifyHeaders",
+            responseHeaders,
+          },
+          condition: {
+            urlFilter: match,
+            resourceTypes: ["script", "stylesheet", "xmlhttprequest", "other"],
+          },
+        },
+      ],
+    });
+  });
